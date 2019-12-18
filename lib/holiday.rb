23fa5b009dddc5469bd3bf31623e751b3ece6432
Joshua Sanders <ninjosh2@gmail.com>
2019-12-18 23:25:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,8 +46,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do | season, holiday| 
     puts "#{season.capitalize}:"
     holiday.each do |holiday, item|
-      puts " #{holiday.to_s.split(" ")}: #{item.join(", ")}"
-    end
+     puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+  end
   end
 end
 
