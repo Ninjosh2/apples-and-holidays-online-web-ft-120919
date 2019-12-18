14b66ce0fa11b17b8ab54b75ce1e73ad5ef711a1
Joshua Sanders <ninjosh2@gmail.com>
2019-12-18 22:27:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,15 +27,6 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
     decorations << supply 
   end
 end 
-  
-  
-  
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-
-end
-
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
